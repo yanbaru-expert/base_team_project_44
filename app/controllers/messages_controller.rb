9b74a2262f9383550492c4914bdc3d72a010ed97
ERIKASHIRAKI <iwdimp@gmail.com>
2020-12-20 20:28:41 +0900

    新規投稿機能を実装しました

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+
   def new
     @message = Message.new
   end
@@ -7,4 +8,8 @@ class MessagesController < ApplicationController
     Message.create(message_params)
   end
 
+  private
+  def message_params
+    params.require(:message).permit(:title,content)
+  end
 end
